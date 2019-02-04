73c01a1038173ad01682787a1dbd908f3618bf1d
Roberto <roberto.lima1@outlook.com>
2019-02-03 23:32:50 -0200

    Alterando controlador

---
+++ BibliotecaTeca/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BibliotecaTeca.Models;
+using BibliotecaTeca.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BibliotecaTeca.Controllers
+{
+
+    public class LivroController : Controller
+    {
+        private readonly ILivroRepository livroRepository;
+
+        public LivroController(ILivroRepository livroRepository)
+        {
+            this.livroRepository = livroRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            Livro livro = new Livro("", 0, new DateTime());            
+            return View();
+        }
+     
+    }
+}
